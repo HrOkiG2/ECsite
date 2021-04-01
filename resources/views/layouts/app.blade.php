a1fd762237862c3c0b05d4fe136495e78e35cc40
HIGA-RYOTA <hokubukankou@gmail.com>
2021-04-01 22:47:42 +0900

    cssの変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,46 +20,39 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <link href = " https://unpkg.com/sanitize.css " rel = "stylesheet " />
     <title>The Promontroy</title>
 </head>
+
 <body>
     <div class="container-fulid">
-        <div class="fixed-top">
-        <!--右寄せ-->
-                
-            <nav class="navbar navbar-expand-xg navbar-light bg-light">
-                <button class="navbar-toggler" type="button"
-                    data-toggle="collapse"
-                    data-target="#navmenu1"
-                    aria-controls="navmenu1"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                @if(Auth::check())
-                <p class="text-left">{{ Auth::user()->name }}さん,ようこそ！</p>
+        <header>
+            <nav class="navbar navbar-dark" style="background-color: #C0C0C0;">
+                <div class="d-flex justify-content-between">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navmenu1" aria-controls="navmenu1" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <a href="{{ url('/')}}" class="navbar-brand nav-bar">The Promontroy</a>
+                    @if(Auth::check())
+                    <p class="nav-bar">{{ Auth::user()->name }}さん,ようこそ！</p>
+                </div>
                 <div class="collapse navbar-collapse" id="navmenu1">
-                    <div class="navbar-nav d-flex">
-                        <!--左寄せ-->
-                        <div class="">
-                            <li>
-                                <a href="/display">
-                                    <img src="image/cart.png" class="navf"> 
-                                    <span>カートを見る</span>
-                                </a>
-                            </li>
-                            <br>
-                            <li>
-                                <a class="" href="{{ route('logout') }}">
-                                    <img src="image/logouto.png" class="navf">
-                                    <span>ログアウト</span>
-                                </a>
-                            </li>
-                            
-                        </div>
-                        
-                @else
+                    <div class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/display">
+                                <img src="image/cart.png" class="navf">
+                                <span>カートを見る</span>
+                            </a>
+                        </li>
+                        <br>
+                        <li class="nab-item">
+                            <a class="" href="{{ route('logout') }}">
+                                <img src="image/logouto.png" class="navf">
+                                <span>ログアウト</span>
+                            </a>
+                        </li>
+                        @else
                         <a href="{{ route('login') }}">
                             <img src="image/login.png" class="navf">
                             <span>ログイン</span>
@@ -69,25 +63,26 @@
                             <span>新規会員登録</span>
                         </a>
                     </div>
-                @endif
+                    @endif
                 </div>
-                    
             </nav>
-        </div>
-        <header class="header">
-            <h1 class="title">
-                <a class="top_back" href="{{ url('/') }}">
+            <div class="title">
+                <h1>
+                    <a class="top_back text-monospace d-flex" href="{{ url('/') }}">
                         {{ config('app.name', 'The　Promontroy') }}
-                </a>
-            </h1>
+                    </a>
+                </h1>
+            </div>
         </header>
     </div>
+
     <!--content-->
     @yield('content')
     @yield('cart')
-    
+
     <div class="conteinar-fulid" style="background-color: skyblue; height:150px;">
-        
+
     </div>
 </body>
+
 </html>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,92 +1,59 @@
 @extends('layouts.app')
 @section('content')
 <div class="container-fluid">
-    <main>
     <div class="commitment">
-        <h2 class="mt-5" style="text-align: center;">The Promontroy<br>３つのこだわり</h2><br>
+        <h2 class="pt-5 pb-5" style="text-align: center;">The Promontroy<br>３つのこだわり</h2><br>
         <div class="contents">
             <div class="row">
-                <div class="element col-sm-6 col-xs-12 col-sm-offset-3 col-md-6 col-md-offset-3">
-                    <img src="{{asset('image/pickup.png')}}" class="pickup img-thumbnail">
+                <div class="element col-xs-12 col-sm-12 col-md-12 col-lg-6 ">
+                    <img src="{{asset('image/pickup.png')}}" class="img-thumbnail">
                 </div>
-                <div class="element col-sm-6 col-xs-12 col-sm-offset-3 col-md-6 col-md-offset-3">
+                <div class="element　col-xs-12 col-sm-12 col-md-12 col-lg-6 ml-2">
                     <p class="elment-child">①「Simple Is Fashionable」をテーマに大人の為のおしゃれをお届けします！</p>
                     <p class="elment-child">②京都発！熟練の職人さんによるこだわりの刺繍を全ての商品に！</p>
                     <p class="elment-child">③全品送料無料！もちろん沖縄も</p>
                 </div>
-                
+
             </div>
         </div>
-    </div> 
-        <!--ピックアップ商品-->
-        <div class="container">
-                <h2 class="mt-3 mb-3" style="text-align: center;">ピックアップ商品</h2>
-                <ul style="text-align: center;">
-                    @foreach($pickups as $pickup)
-                    <li class="contents">
-                        <div class="row a　d-flex">
-                            <div class="col-lg-6 col-xs-12 col-sm-12">
-                                <p class="mt-3 mb-3">
-                                    <img src="{{asset('image/'.$pickup->imgpath)}}" class="img-thumbnail">
-                                </p>
-                            </div>
-                            <div class="col-lg-6 col-xs-12 col-sm-12">
-                                <h3 class="mt-3 mb-3">{{$pickup->product_name}}</h3>
-                                <p>{{$pickup->detail}}</p>
-                                <p>{{$pickup->price}}</p>
-
-                                <!--カートに追加-->
-                                <form action="mycart" method="POST">
-                                    @csrf
-                                    <input type="hidden" name="product" value="{{$pickup->product_id}}">
-                                    <input type="submit" class="btn btn-secondary btn-lg btn-block" value="カートに追加">
-                                </form>
-                            </div>
-                        </div>
-                    </li>
-                    
-                    @endforeach
-                </ul>
-        </div>
-
-        <!--商品一覧-->
-        <div class="conteinar">
-        <h2 class="mt-3 mb-3" style="text-align: center;">商品一覧</h2>
-            <div class="d-flex justify-content-center  row col-xs-12 col-sm-12 p-5">
-                @foreach($datas as $data)
-                <div class="card center-block" style="width: 25rem; margin:10px; text-align: center;">
-                    <div class="card-img-top">
-                        <img src="{{asset('image/'.$data->imgpath)}}" class="img-thumbnail" style="width: 100%; height:250px;">
-                        
-                    </div>
-                    <div class="card-body">
-                        <h3>{{$data->product_name}}</h3>
-                        <h4>{{$data->price}}円</h4>
-                        <br>
-                        <!--商品詳細-->
-                        <div class="">
-                            <form action="product" method="POST" style="margin-bottom: 5px;">
-                                @csrf
-                                <input type="hidden" name="product" value="{{$data->product_id}}">
-                                <input type="submit" class="btn btn-primary btn-lg btn-block" value="詳細を見る">
-                            </form>
-                            
-                            <!--カートに追加-->
-                            <form action="mycart" method="POST">
-                                @csrf
-                                <input type="hidden" name="product" value="{{$data->product_id}}">
-                                <input type="submit" class="btn btn-secondary btn-lg btn-block" value="カートに追加">
-                            </form>
-                        </div>
-                    </div>
+    </div>
+</div>
+<!--ピックアップ商品-->
+<div class="container-fulid">
+    <div class="pickup">
+        <h2 class="pt-5 pb-5" style="text-align: center;">ピックアップ商品</h2>
+        @foreach($pickups as $pickup)
+        <li class="contents">
+            <div class="row a　d-flex">
+                <div class="col-lg-6 col-xs-12 col-sm-12 mt-3 mb-3">
+                    <img src="{{asset('image/'.$pickup->imgpath)}}" class="img-thumbnail" style="width: 80%; margin:0 auto;">
                 </div>
-                @endforeach
+                <div class="col-lg-6 col-xs-12 col-sm-12">
+                    <h3 class="mt-3 mb-3">{{$pickup->product_name}}</h3>
+                    <p>{{$pickup->detail}}</p>
+                    <p>{{$pickup->price}}</p>
+
+                    <!--カートに追加-->
+                    <form action="mycart" method="POST" class="pb-5">
+                        @csrf
+                        <input type="hidden" name="product" value="{{$pickup->product_id}}">
+                        <input type="submit" class="btn btn-secondary btn-lg btn-block" value="カートに追加">
+                    </form>
+                </div>
             </div>
-            <div class="d-flex justify-content-center">
-                {{$datas->links()}}
-            </div>
+        </li>
+        @endforeach
+    </div>
+</div>
+
+<!--商品一覧-->
+<div class="conteinar-fulid list">
+    
+        <h2 class="pt-5 pb-5" style="text-align: center;">商品一覧</h2>
+        <div class="d-flex justify-content-center">
+            {{$datas->links()}}
         </div>
-    </main>
+    
 </div>
 
 <script src="{{ asset('/js/sample.js') }}"></script>
